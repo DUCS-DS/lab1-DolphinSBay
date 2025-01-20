f28c08686c77a7530603629b53babedfc41c6b98
DolphinSBay <jan.dueppe@gmail.com>
2025-01-20 17:13:39 -0600

    Update edit_me.py

--- edit_me.py
+++ edit_me.py
@@ -3,13 +3,12 @@ def find_max(lst):
     """return the maximum element"""
 
     current_max = lst[0]
-
-    #
-    #  YOUR CODE GOES HERE
-    #
-
+    for i in range(1, len(lst)):
+        if lst[i] > current_max:
+            current_max = lst[i]
     return current_max
 
 test_list = [2112*i % 2024 for i in range(10000)]
 
 print(find_max(test_list))
+ # Jan Dueppe
